e9a412c483adb6db414b444d105dc3189ed904b0
bbbbbr <bbbbbr@users.noreply.github.com>
2022-05-30 14:23:38 -0700

    Lang: CA: dictionary and answer list updates from @alvaromartinezmajado

--- src/lang_ca/answerbitmap_rle.h
+++ src/lang_ca/answerbitmap_rle.h
@@ -1,7 +1,7 @@
 
 
-#define rle_answerBitmapIndex_sz_comp 547
-#define rle_answerBitmapIndex_sz_decomp 1378
+#define rle_answerBitmapIndex_sz_comp 1364
+#define rle_answerBitmapIndex_sz_decomp 1383
 
 
 extern const unsigned char rle_answerBitmapIndex[];
